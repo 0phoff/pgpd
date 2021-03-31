238e109517be3cc97b5374cd3c8d6e1d497778cf
0phoff <0phoff@users.noreply.github.com>
2021-03-31 17:00:57 +0200

    Github workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages',
     'sphinx_toggleprompt',
     'nbsphinx',
     'matplotlib.sphinxext.plot_directive',
